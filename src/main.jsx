923d0f618ba6da7b6a13d90614da338bbbc7311d
gricius <gricius@gmail.com>
2023-10-20 00:42:58 +0200

    6c

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit' 
 import App from './App'
+import noteService from './services/notes.js'
+import noteReducer, { setNotes } from './reducers/noteReducer'
 
-import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
 const store = configureStore({
@@ -13,6 +14,10 @@ const store = configureStore({
     filter: filterReducer
   }
 })
+
+noteService.getAll().then(notes =>
+  store.dispatch(setNotes(notes))
+)
 
 console.log(store.getState())
 
